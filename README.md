856128efcb4644d5f764f937f6518c01c100895f
sumtype <james@sumtype.tech>
2017-12-21 03:22:53 -0500

    Added neural style transfer implementation.  Updated README.md.

--- README.md
+++ README.md
@@ -1,3 +1,72 @@
 # Neural Style Transfer
 
-Neural style transfer implementation in TensorFlow with a couple alterations and improvements.
+Neural style transfer implementation in TensorFlow with a couple improvements and quirks.  Based on [A Neural Algorithm of Artistic Style](https://arxiv.org/pdf/1508.06576.pdf) and improvements in [Improving the Neural Algorithm of Artistic Style](https://arxiv.org/pdf/1605.04603.pdf).
+
+### Improvements
+
+* Geometric weighting scheme for content and style layers.
+* Utilizes all 16 convolutional layers in the VGG-19 model.
+* Calculates Gram matrices using shifted activations.
+* Feature comparisons with blurred chain correlations between local layers.
+
+### Quirks
+
+* Weighs feature comparisons within the same layer, *l*, and *l* and *l - 1* differently.
+* Blurs *l* and *l - 1* correlation using a gaussian blur instead of a box blur.
+* Hyperparameters...
+
+### Usage
+
+Download some images to work with and the pretrained [VGG-19](http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-19.mat) model (save the model file in the project's root directory, don't change it's name).  Install [NumPy](https://www.scipy.org/install.html), [SciPy](https://www.scipy.org/install.html), and [TensorFlow](https://www.tensorflow.org/install/).  If possible, install the version of TensorFlow that runs on your GPU, this speeds things up considerably.  Then run the following in your terminal:
+
+$`python3 neural-style-transfer.py [content path] [style path] [width]`
+
+Generated images are saved inside the "output" directory when the process is completed.  Inside the "iterations" folder there will be generated images from every 100 iterations processed.  Inside the "setup" folder will be the resized content and style images used during processing.
+
+### Examples
+
+Content | Style | Output
+------- | ----- | ------
+![Sentinel Rock Yosemite](/readme-files/sentinel-rock-yosemite.jpg) | ![The Beeches](/readme-files/the-beeches.jpg) | ![Sentinel Rock Yosemite + The Beeches](/readme-files/sentinel-rock-yosemite+the-beeches.jpg)
+
+Iterations: 1000
+
+Content: [Sentinel Rock, Yosemite](https://images.metmuseum.org/CRDImages/ph/original/DP152226.jpg) (attributed to Carleton E. Watkins, ca. 1872, printed ca. 1876)
+
+Style: [The Beeches](https://images.metmuseum.org/CRDImages/ap/original/DT75.jpg) (Asher Brown Durand, 1845)
+
+</br>
+
+Content | Style | Output
+------- | ----- | ------
+![Man in a Forest Landscape](/readme-files/man-in-a-forest-landscape.jpg) | ![Cypresses](/readme-files/cypresses.jpg) | ![Man in a Forest Landscape + Cypresses](/readme-files/man-in-a-forest-landscape+cypresses.jpg)
+
+Iterations: 1000
+
+Content: [Man in a Forest Landscape](https://images.metmuseum.org/CRDImages/ph/original/DT223780.jpg) (Constant Alexandre Famin, ca. 1870)
+
+Style: [Cypresses](https://images.metmuseum.org/CRDImages/ep/original/DP130999.jpg) (Vincent van Gogh, 1889)
+
+</br>
+
+Content | Style | Output
+------- | ----- | ------
+![Medinet Habu](/readme-files/medinet-habu.jpg) | ![Rochishin Chopping Off the Head of Nio](/readme-files/rochishin-chopping-off-the-head-of-nio.jpg) | ![Medinet Habu + Rochishin Chopping Off the Head of Nio](/readme-files/medinet-habu+rochishin-chopping-off-the-head-of-nio.jpg)
+
+Iterations: 1000
+
+Content: [Medinet-Habu](https://images.metmuseum.org/CRDImages/ph/original/DT1163.jpg) (John Beasley Greene, 1854)
+
+Style: [Rochishin Chopping Off the Head of Nio](https://images.metmuseum.org/CRDImages/as/original/56_121_40_162330.jpg) (School of Katsushika Hokusai, 18th-19th century)
+
+</br>
+
+Content | Style | Output
+------- | ----- | ------
+![11:00 A.M. Monday, May 9th, 1910. Newsies at Skeeter's Branch, Jefferson near Franklin. They were all smoking. Location: St. Louis, Missouri.](/readme-files/11-00am-monday-may-9th-1910-newsies-at-skeeters-branch-jefferson-near-franklin-they-were-all-smoking-location-st-louis-missouri.jpg) | ![Fleur de Lis](/readme-files/fleur-de-lis.jpg) | ![11:00 A.M. Monday, May 9th, 1910. Newsies at Skeeter's Branch, Jefferson near Franklin. They were all smoking. Location: St. Louis, Missouri. + Fleur de Lis](/readme-files/11-00am-monday-may-9th-1910-newsies+fleur-de-lis.jpg)
+
+Iterations: 1000
+
+Content: [11:00 A.M. Monday, May 9th, 1910. Newsies at Skeeter's Branch, Jefferson near Franklin. They were all smoking. Location: St. Louis, Missouri.](https://images.metmuseum.org/CRDImages/ph/original/DP352686.jpg) (Lewis Hine, May 9, 1910)
+
+Style: [Fleur de Lis](https://images.metmuseum.org/CRDImages/ap/original/DP167061.jpg) (Robert Reid, ca. 1895-1900)
---
+++ neural-style-transfer.py
@@ -0,0 +1,145 @@
+import sys
+import scipy.io
+import scipy.misc
+from scipy.ndimage.filters import gaussian_filter
+import numpy as np
+import tensorflow as tf
+
+def loadVggModel(path, image):
+    vgg = scipy.io.loadmat(path)
+    def weights(layer, expectedLayerName):
+        assert vgg['layers'][0][layer][0][0][0][0] == expectedLayerName
+        return vgg['layers'][0][layer][0][0][2][0][0], vgg['layers'][0][layer][0][0][2][0][1]
+    def conv2D(prevLayer, layer, layerName):
+        w, b = weights(layer, layerName)
+        return tf.nn.conv2d(prevLayer, filter=tf.constant(w), strides=[1, 1, 1, 1], padding='SAME') + tf.constant(np.reshape(b, (b.size)))
+    graph = {}
+    graph['input'] = tf.Variable(np.zeros((1, image.shape[1], image.shape[2], image.shape[3])), dtype = 'float32')
+    graph['conv1_1'] = tf.nn.relu(conv2D(graph['input'], 0, 'conv1_1'))
+    graph['conv1_2'] = tf.nn.relu(conv2D(graph['conv1_1'], 2, 'conv1_2'))
+    graph['maxpool1'] = tf.nn.max_pool(graph['conv1_2'], ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    graph['conv2_1'] = tf.nn.relu(conv2D(graph['maxpool1'], 5, 'conv2_1'))
+    graph['conv2_2'] = tf.nn.relu(conv2D(graph['conv2_1'], 7, 'conv2_2'))
+    graph['maxpool2'] = tf.nn.max_pool(graph['conv2_2'], ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    graph['conv3_1'] = tf.nn.relu(conv2D(graph['maxpool2'], 10, 'conv3_1'))
+    graph['conv3_2'] = tf.nn.relu(conv2D(graph['conv3_1'], 12, 'conv3_2'))
+    graph['conv3_3'] = tf.nn.relu(conv2D(graph['conv3_2'], 14, 'conv3_3'))
+    graph['conv3_4'] = tf.nn.relu(conv2D(graph['conv3_3'], 16, 'conv3_4'))
+    graph['maxpool3'] = tf.nn.max_pool(graph['conv3_4'], ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    graph['conv4_1'] = tf.nn.relu(conv2D(graph['maxpool3'], 19, 'conv4_1'))
+    graph['conv4_2'] = tf.nn.relu(conv2D(graph['conv4_1'], 21, 'conv4_2'))
+    graph['conv4_3'] = tf.nn.relu(conv2D(graph['conv4_2'], 23, 'conv4_3'))
+    graph['conv4_4'] = tf.nn.relu(conv2D(graph['conv4_3'], 25, 'conv4_4'))
+    graph['maxpool4'] = tf.nn.max_pool(graph['conv4_4'], ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    graph['conv5_1'] = tf.nn.relu(conv2D(graph['maxpool4'], 28, 'conv5_1'))
+    graph['conv5_2'] = tf.nn.relu(conv2D(graph['conv5_1'], 30, 'conv5_2'))
+    graph['conv5_3'] = tf.nn.relu(conv2D(graph['conv5_2'], 32, 'conv5_3'))
+    graph['conv5_4'] = tf.nn.relu(conv2D(graph['conv5_3'], 34, 'conv5_4'))
+    graph['maxpool5'] = tf.nn.max_pool(graph['conv5_4'], ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    return graph
+
+def generateNoiseImage(contentImage, noiseRatio = 0.6):
+    noiseImage = np.random.uniform(-20, 20, (1, contentImage.shape[1], contentImage.shape[2], contentImage.shape[3])).astype('float32')
+    return noiseImage * noiseRatio + contentImage * (1 - noiseRatio)
+
+def computeContentCost(model, contentLayers, loss = 0):
+    for layer, weight in contentLayers:
+        loss += weight * computeContentLayerCost(sess.run(model[layer]), model[layer])
+    return loss
+
+def computeContentLayerCost(aC, aG):
+    _, nH, nW, nC = aG.get_shape().as_list()
+    return (1 / (4 * nH * nW * nC)) * tf.reduce_sum(tf.square(tf.subtract(aC, aG)))
+
+def gramMatrix(l, k, shift = -1.):
+    return tf.matmul(tf.add(l, tf.constant(shift)), tf.transpose(tf.add(k, tf.constant(shift)), perm=[1, 0]))
+
+def computeStyleLayerCost(l, model, layers, loss = 0):
+    gL = tf.stack(model[layers[l][0]])
+    gK = tf.stack(model[layers[l - 1][0]])
+    sL = tf.stack(sess.run(model[layers[l][0]]))
+    sK = tf.stack(sess.run(model[layers[l - 1][0]]))
+    _, lH, lW, lC = gL.get_shape().as_list()
+    sK = tf.transpose(tf.image.resize_images(tf.transpose(sK, perm = [0, 3, 2, 1]), [lC, lW]), perm = [0, 3, 2, 1]) if tf.transpose(sL, perm = [0, 3, 2, 1]).shape != tf.transpose(sK, perm = [0, 3, 2, 1]).shape else sK
+    gK = tf.transpose(tf.image.resize_images(tf.transpose(gK, perm = [0, 3, 2, 1]), [lC, lW]), perm = [0, 3, 2, 1]) if tf.transpose(gL, perm = [0, 3, 2, 1]).shape != tf.transpose(gK, perm = [0, 3, 2, 1]).shape else gK
+    sK = tf.image.resize_images(sK, [lH, lW]) if sL.shape != sK.shape else sK
+    gK = tf.image.resize_images(gK, [lH, lW]) if gL.shape != gK.shape else gK
+    sK = tf.stack(gaussian_filter(sK.eval(), sigma = 0.5))
+    gK = tf.stack(gaussian_filter(gK.eval(), sigma = 0.5))
+    l2kWeight = 0.25
+    l2lWeight = 0.75
+    GS = tf.add(tf.multiply(gramMatrix(tf.transpose(tf.reshape(sL, [lH * lW, lC])), tf.transpose(tf.reshape(sK, [lH * lW, lC]))), l2kWeight), tf.multiply(gramMatrix(tf.transpose(tf.reshape(sL, [lH * lW, lC])), tf.transpose(tf.reshape(sL, [lH * lW, lC]))), l2lWeight))
+    GG = tf.add(tf.multiply(gramMatrix(tf.transpose(tf.reshape(gL, [lH * lW, lC])), tf.transpose(tf.reshape(gK, [lH * lW, lC]))), l2kWeight), tf.multiply(gramMatrix(tf.transpose(tf.reshape(gL, [lH * lW, lC])), tf.transpose(tf.reshape(gL, [lH * lW, lC]))), l2lWeight))
+    loss += (1 / (4 * (lC ** 2) * ((lH * lW) ** 2))) * tf.reduce_sum(tf.square(tf.subtract(GS, GG)))
+    return loss
+
+def computeStyleCost(model, styleLayers, loss = 0):
+    for l in range(1, len(styleLayers)):
+        layerWeight = styleLayers[l][1]
+        loss += layerWeight * computeStyleLayerCost(l, model, styleLayers)
+    return loss
+
+def getWeightedContentLayers(layers, output = []):
+    for i in range(len(layers)): output += [(layers[i], 2**(i + 1))]
+    return output
+
+def getWeightedStyleLayers(layers, output = []):
+    for i in range(len(layers)): output += [(layers[i], 2**(len(layers) - (i + 1)))]
+    return output
+
+def loadImage(image, width = None, height = None):
+    image = np.reshape(image, ((1,) + image.shape))
+    image = image - np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3))
+    if height != None and width != None: return tf.cast(tf.image.resize_images(image, [height, width]), tf.float32)
+    elif width != None:
+        height = tf.cast((tf.cast(image.shape[1], tf.int32) / tf.cast(image.shape[2], tf.int32)) * width, tf.int32).eval()
+        return tf.cast(tf.image.resize_images(image, [height, width]), tf.float32)
+    else: return tf.cast(tf.image.resize_images(image, [tf.cast(image.shape[1], tf.int32), tf.cast(image.shape[2], tf.int32)]), tf.float32)
+
+def saveImage(path, image):
+    image = image + np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3))
+    scipy.misc.imsave(path, np.clip(image[0], 0, 255).astype('uint8'))
+
+#Setup
+tf.reset_default_graph()
+sess = tf.InteractiveSession()
+content = loadImage(scipy.misc.imread(sys.argv[1]), int(sys.argv[3]))
+style = loadImage(scipy.misc.imread(sys.argv[2]), tf.cast(content.shape[2], tf.int32), tf.cast(content.shape[1], tf.int32))
+output = generateNoiseImage(content)
+
+#Save base images for reference.
+saveImage('output/setup/content.jpg', content.eval())
+saveImage('output/setup/style.jpg', style.eval())
+
+model = loadVggModel('imagenet-vgg-verydeep-19.mat', content)
+modelLayers = ['conv1_1', 'conv1_2', 'conv2_1', 'conv2_2', 'conv3_1', 'conv3_2', 'conv3_3', 'conv3_4', 'conv4_1', 'conv4_2', 'conv4_3', 'conv4_4', 'conv5_1', 'conv5_2', 'conv5_3', 'conv5_4']
+contentLayers = getWeightedContentLayers(modelLayers)
+styleLayers = getWeightedStyleLayers(modelLayers)
+
+#Content Cost
+sess.run(model['input'].assign(content))
+jContent = computeContentCost(model, contentLayers)
+
+#Style Cost
+sess.run(model['input'].assign(style))
+jStyle = computeStyleCost(model, styleLayers)
+
+#Total Cost and Optimizer
+j = 10 * jContent + 1 * jStyle # (#) = alpha, (#) = beta
+optimizer = tf.train.AdamOptimizer(2.0).minimize(j)
+
+def neuralStyleTransfer(sess, input, iterations = 1000):
+    sess.run(tf.global_variables_initializer())
+    output = sess.run(model['input'].assign(input))
+    for i in range(iterations):
+        sess.run(optimizer)
+        output = sess.run(model['input'])
+        t, c, s = sess.run([j, jContent, jStyle])
+        print('Iteration: ' + str(i))
+        print('    Style Cost: ' + str(s))
+        print('    Content Cost: ' + str(c))
+        print('    Total Cost: ' + str(t))
+        if i % 100 == 0: saveImage('output/iterations/' + str(i) + '.jpg', output)
+    saveImage('output/output.jpg', output)
+
+neuralStyleTransfer(sess,output)
